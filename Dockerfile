24e3392a22d429e84860f0653d153236a441430c
Panos Vlachos <panosvlchs@gmail.com>
2023-01-28 15:12:06 +0000

    Added basic docker files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10-bullseye
+
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /PANOpticon
+WORKDIR /PANOpticon
+COPY . .
+# RUN apt add --update --no-cache postgresql-client jpeg-dev
+# RUN apt add --update --no-cache --virtual .tmp-build-deps gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
+RUN pip install -r requirements.txt
--- panopticon/settings.py
+++ panopticon/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-as(#&iurnz1o*@^!nx*7rwkw1geer2@87g_on(l-ii&rz3os)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,20 @@ WSGI_APPLICATION = 'panopticon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'HOST': os.environ.get('DB_HOST'),
+    'NAME': os.environ.get('DB_NAME'),
+    'USER': os.environ.get('DB_USER'),
+    'PASSWORD': os.environ.get('DB_PASS'),
+  }
 }
 
 
