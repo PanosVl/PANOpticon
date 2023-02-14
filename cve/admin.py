4a14432f70fb57507ae355cce73319c9aad6787f
Panos Vlachos <panosvlchs@gmail.com>
2023-02-14 06:55:05 +0000

    Added Vulnerability model to admin

--- cve/admin.py
+++ cve/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from cve.models import *
 
-# Register your models here.
+class VulnerabilityAdmin(admin.ModelAdmin):
+    list_display = ('cve_id', 'epss', 'actively_exploited')
+    search_fields = ('cve_id', 'epss', 'actively_exploited')
+
+admin.site.register(Vulnerability, VulnerabilityAdmin)
