dc06bc3942d50056d56420a4cb57de22d377b650
Panos Vlachos <panosvlchs@gmail.com>
2023-02-18 21:15:20 +0000

    Added all Vuln fields in Admin list

--- cve/admin.py
+++ cve/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from cve.models import *
 
 class VulnerabilityAdmin(admin.ModelAdmin):
-    list_display = ('cve_id', 'epss', 'actively_exploited')
-    search_fields = ('cve_id', 'epss', 'actively_exploited')
+    list_display = ('cve_id', 'epss', 'pulses', 'actively_exploited', 'date_discovered')
+    search_fields = ('cve_id', 'epss', 'pulses', 'actively_exploited', 'date_discovered')
 
 admin.site.register(Vulnerability, VulnerabilityAdmin)
