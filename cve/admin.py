37f19060c5bd7a1422aec21c2ea322517e91708b
Panos Vlachos <panosvlchs@gmail.com>
2023-02-23 19:50:20 +0000

    Override ModelAdmin to create a custom ReadOnly class for Vulnerability

--- cve/admin.py
+++ cve/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from cve.models import *
 
-class VulnerabilityAdmin(admin.ModelAdmin):
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+    actions = None
+    list_display_links = ['cve_id']
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def get_readonly_fields(self, request, obj=None):
+        # Make all fields read-only
+        return [field.name for field in obj._meta.fields]
+
+class VulnerabilityAdmin(ReadOnlyAdmin):
     list_display = ('cve_id', 'epss', 'pulses', 'actively_exploited', 'date_discovered')
     search_fields = ('cve_id', 'epss', 'pulses', 'actively_exploited', 'date_discovered')
 
