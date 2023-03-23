b7884a9aff1e9e97191c322a0400766c7ebad2b2
PanosVl <panosvlchs@gmail.com>
2023-03-23 16:44:15 +0000

    Added new fields

--- cve/models.py
+++ cve/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Vulnerability(models.Model):
     id = models.AutoField(primary_key=True)
     cve_id = models.CharField(max_length=20, unique=True)
-    epss = models.CharField(max_length=20)
+    epss = models.CharField(max_length=20, null=True)
+    cvss = models.CharField(max_length=20, null=True)
+    attack_vector = models.CharField(max_length=20, null=True)
     pulses = models.IntegerField(default=0, null=True)
     date_discovered = models.DateField(default=None, null=True)
     KEV = models.BooleanField(default=False)
