7629a2dc58f7f8378305d4f0dc8fc46a52da01c6
Ubuntu <panoptis@panopticon-host.altk2hy3ggbexgmnswka30zxmf.zx.internal.cloudapp.net>
2023-03-30 16:19:13 +0000

    Added exception handling for CVSS v3.1

--- cve/utils/api_fetching.py
+++ cve/utils/api_fetching.py
@@ -4,6 +4,7 @@ from cve.models import *
 import datetime
 import os
 from pyExploitDb import PyExploitDb
+import time
 
 # Initialize clone of exploit-db
 pEdb = PyExploitDb()
@@ -39,8 +40,8 @@ def get_basic_CVE_info_NVD(cve):
     """
     NVD NIST API to get basic information on a given CVE
     """
-    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={cve}&apiKey={os.environ['NVD_API_KEY']}"
-    r = requests.get(url)
+    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={cve}"    
+    r = requests.get(url, headers={'apiKey': os.environ['NVD_API_KEY']})
     if r.ok:
         return r.json()['vulnerabilities'][0]
     else:
@@ -64,20 +65,32 @@ def get_all_KEV_NVD():
     """
     Queries NVD NIST API to get all Known Exploited Vulnerabilities (KEV) and write them in db
     """
-    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?hasKev&apiKey={os.environ['NVD_API_KEY']}"
-    response = requests.get(url)
+    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?hasKev&"    
+    response = requests.get(url, headers={'apiKey': os.environ['NVD_API_KEY']})
     data = response.json()
     for item in data['vulnerabilities']:
         if not Vulnerability.objects.filter(cve_id=item['cve']['id']).exists():
-            nvd_data = get_basic_CVE_info_NVD(item['cve'])
+            nvd_data = get_basic_CVE_info_NVD(item['cve']['id'])
             if nvd_data:
+                try:
+                    cvss = nvd_data['cve']['metrics']['cvssMetricV31']['cvssData']['baseScore'],
+                    attack_vector = nvd_data['cve']['metrics']['cvssMetricV31']['cvssData']['attackVector'],
+                except KeyError:  # it didn't find CVSS v3.1, try for v2
+                    try:
+                        cvss = nvd_data['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseScore'],
+                        attack_vector = nvd_data['cve']['metrics']['cvssMetricV2'][0]['cvssData']['attackVector'],
+                    except KeyError:  # it didn't find CVSS v2, print out what you found and move on
+                        print(nvd_data['cve']['metrics'].keys())
+                        print(item['cve']['id'])
+                        cvss = attack_vector = None
                 Vulnerability.objects.create(
                     cve_id = item['cve']['id'],
                     epss = get_EPSS(item['cve']['id']),
-                    cvss = nvd_data['metrics']['cvssMetricV31']['cvssData']['baseScore'],
-                    attack_vector = nvd_data['metrics']['cvssMetricV31']['cvssData']['attackVector'],
                     pulses = OTX_pulse(item['cve']['id']),
+                    cvss = cvss,
+                    attack_vector = attack_vector,
                     date_discovered = datetime.datetime.strptime(item['cve']['published'].split('T')[0], '%Y-%m-%d').date(),
                     KEV = True,
                     exploit_db = exploit_db_poc(item['cve']['id'])
                 )
+            time.sleep(6)  # Add 6" sleep to avoid being throttled out by NIST's API
