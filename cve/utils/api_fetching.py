ea84ee6920fbe9555fb2a440bf40c7ed6a5d0df9
Panos Vlachos <panosvlchs@gmail.com>
2023-04-17 03:13:07 +0100

    Added util to get efficiency & coverage statistics for CVSS and EPSS based metrics

--- cve/utils/api_fetching.py
+++ cve/utils/api_fetching.py
@@ -115,3 +115,28 @@ def get_all_KEV_NVD():
             logger.error(f"epss: {get_EPSS(item['cve']['id'])}")
             logger.error(f"CVSS: {cvss}")
             logger.error(f"Attack Vector: {attack_vector}")
+
+def get_statistics():
+    """
+    Returns Efficiency & Coverage compared to CVSS and EPSS based metrics. 
+    Efficiency = TP / (TP + FP)
+    Coverage = TP / (TP + FN)
+    TP = True Positive
+    FP = False Positive
+    FN = False Negative
+    We assume prioritization is done on CVSS >= 7 and EPSS >= 0.150
+    """
+    tp_cvss = Vulnerability.objects.filter(cvss__gte=7, KEV=True).count()
+    fp_cvss = Vulnerability.objects.filter(cvss__gte=7, KEV=False).count()
+    fn_cvss = Vulnerability.objects.filter(cvss__lt=7, KEV=True).count()
+    tp_epss = Vulnerability.objects.filter(epss__gte=0.150, KEV=True).count()
+    fp_epss = Vulnerability.objects.filter(epss__gte=0.150, KEV=False).count()
+    fn_epss = Vulnerability.objects.filter(epss__lt=0.150, KEV=True).count()
+    efficiency_cvss = (tp_cvss / (tp_cvss + fp_cvss)) * 100
+    coverage_cvss = (tp_cvss / (tp_cvss + fn_cvss)) * 100
+    efficiency_epss = (tp_epss / (tp_epss + fp_epss)) * 100
+    coverage_epss = (tp_epss / (tp_epss + fn_epss)) * 100
+    print(f"CVSS Efficiency: {efficiency_cvss} %")
+    print(f"EPSS Efficiency: {efficiency_epss} %")
+    print(f"CVSS Coverage: {coverage_cvss} %")
+    print(f"CVSS Coverage: {coverage_epss} %")
